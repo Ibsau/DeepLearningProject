e05896306158623f0d3120679ad2981457b94f90
Shai Verma <shaiverma@Shais-MacBook-Air.local>
2025-04-23 21:38:44 -0400

    added inflation rates

--- Data/inflation_rate.py
+++ Data/inflation_rate.py
@@ -2,7 +2,7 @@ import pandas as pd
 # Source: Bureau of Labor Statistics
 
 # Load the Excel file
-df = pd.read_excel("/Users/shaiverma/Documents/CSE4095/DeepLearningProject/SeriesReport-20250417191516_70894a.xlsx")
+df = pd.read_excel("/Users/shaiverma/Documents/CSE4095/DeepLearningProject/SeriesReport-20250423210243_1879f5.xlsx")
 
 # Save as CSV
 df.to_csv("inflation_rates.csv", index=False)
